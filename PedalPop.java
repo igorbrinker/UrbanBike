183e61228786ec456dcb6442147e758a8c81fb47
igorgti <44041211+igorgti@users.noreply.github.com>
2019-06-25 10:15:53 -0300

    Update PedalPop.java

--- PedalPop.java
+++ PedalPop.java
@@ -1,38 +1,48 @@
 package br.unilasalle;
 
-public class PedalPremium extends UrbanBike implements Imprimivel{
-	private double limite;
-	
-	
-	protected double getLimite() {
-		return limite;
-	}
+public class PedalPop extends UrbanBike implements Imprimivel{
+	private double taxa;
+	private double aux;
 	
 	@Override
 	protected double creditar(double creditar) {
-		carteira = carteira + creditar;
+		this.addCredito = creditar;
+		
+		if(carteira < creditar + (creditar * taxa)) {
+			System.out.println("Valor creditado insuficiente! Favor, colocar mais creditos...");
+		}else carteira = (carteira + creditar) - (creditar * taxa);
+		
+		if(creditar >= 100) {
+			System.out.println("Parabens, voce agora eh premium!");
+			premium = true;
+		}else premium = false;
+		
 		return carteira;
 	}
 	
 	@Override
 	protected double pedalar(double pedalar) {
-		if(carteira == 0) {
-			System.out.println("Na proxima pedalada voce usara seus creditos premium...");
-			carteira = carteira - pedalar;
+		this.km = pedalar;
+		if(premium != true){
+			if(carteira < pedalar + (pedalar * taxa)) {
+				System.out.println("Valor creditado insuficiente! Favor, colocar mais creditos...");
+			}else carteira = carteira - (pedalar + (pedalar * taxa));
 		}
-		if(carteira == limite) {
-			System.out.println("Voce usou todo o seu limite premium! Recarregue novamente para poder pedalar mais.");
-		}else carteira = carteira - pedalar;
-		
 		return carteira;
+	}
+	
+	protected void setTaxa(double tax) {
+		this.taxa = tax;
+		taxa = taxa/100;
+		aux = taxa;
+		System.out.println("Voce aplicou uma taxa de " + aux *100 + "%, sobre compras e vendas!");
 	}
 
 	public void mostraDados() {
-		if(premium != false) {
+		if(premium != true) {
 			System.out.println("ID: " + getNumeroConta() + ".");
 			System.out.println("Saldo: R$" + getCarteira() + ".");
-			System.out.println("Limite: R$" + getLimite() + ".");
-			System.out.println("Tipo de conta: Premium.");
+			System.out.println("Tipo de conta: POP.");
 		}
 	}
 }
