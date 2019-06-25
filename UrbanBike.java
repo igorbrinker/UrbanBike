a6c7d404653c5ad720f50b1ddb93f569b8fe4f32
igorgti <44041211+igorgti@users.noreply.github.com>
2019-06-25 10:17:06 -0300

    Update UrbanBike.java

--- UrbanBike.java
+++ UrbanBike.java
@@ -5,10 +5,24 @@ public abstract class UrbanBike {
 	protected double carteira;
 	protected boolean premium;
 	protected double addCredito;
+	protected double km;
+	
+	protected int getNumeroConta() {
+		return numeroDaConta;
+	}
+	
+	protected double getCarteira() {
+		return carteira;
+	}
+	
+	protected boolean getPremium() {
+		return premium;
+	}
 	
 	//Gasta creditos
 	protected abstract double pedalar(double pedalar);
 	
 	//Recarrega creditos
 	protected abstract double creditar(double creditar);
+	
 }
